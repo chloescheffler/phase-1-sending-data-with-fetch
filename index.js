bb865a0f146e42636df7705cfb9dc673044c7346
Chloe Scheffler <chloescheffler@gmail.com>
2022-09-07 12:52:37 -0400

    commit tree 20a2e9862a63b4cecf1423f69a7c5ba865ef6c55

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// const labData = 
+// name: 'Steve',
+// email:  'steve@steve.com',
+// ;
+
+function submitData(labData) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(labData = {
+            name: 'Steve',
+            email:  'steve@steve.com',
+            })
+    })
+    .then(res => res.json())
+    .then(object => document.querySelector('body').append(object.id))
+    .catch(error => document.querySelector('body').append(error.message))
+    }
+
